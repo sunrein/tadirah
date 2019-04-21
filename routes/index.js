a9cc5df3f958f819a60df24f04d78e8b4e0922bc
sunrein <sabrina.unrein@gmail.com>
2019-04-21 11:30:42 -0400

    get routes working

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'TaDiRAH' });
 });
 
+router.get('/research-activities', function(req, res, next){
+  res.render('research-activities', { title: 'Research Activites'} );
+ });
+
+router.get('/research-objects', function(req, res, next){
+  res.render('research-objects', { title: 'Research Objects'} );
+ });
+
+router.get('/research-techniques', function(req, res, next){
+  res.render('research-techniques', { title: 'Research Techniques'} );
+ });
+
 module.exports = router;
