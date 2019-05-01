8e6e73c4f1989559b1cf4059e541a32357ae4b7d
sunrein <sabrina.unrein@gmail.com>
2019-05-01 09:37:01 -0400

    pushing to master like a rebel

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,19 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/research-activities', function(req, res, next){
-  res.render('research-activities', { title: 'Research Activites'} );
+  res.render('research-activities', { title: 'Research Activites' } );
  });
 
 router.get('/research-objects', function(req, res, next){
-  res.render('research-objects', { title: 'Research Objects'} );
+  res.render('research-objects', { title: 'Research Objects' } );
  });
 
 router.get('/research-techniques', function(req, res, next){
-  res.render('research-techniques', { title: 'Research Techniques'} );
+  res.render('research-techniques', { title: 'Research Techniques' } );
+ });
+
+router.get('/resources', function(req, res, next){
+  res.render('resources', { title: 'Resources' } );
  });
 
 module.exports = router;
